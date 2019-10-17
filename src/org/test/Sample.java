f8e6b964c6d1565d071b0b82544331a374bf22b8
Dhanalakshmi03 <dhanuram03@gmail.com>
2019-10-17 07:22:59 -0700

    first commit

---
+++ src/org/test/Sample.java
@@ -0,0 +1,20 @@
+package org.test;
+
+public class Sample {
+  
+	 private void oracle() {
+		System.out.println("oracle");
+
+	}
+	 
+	 private void java() {
+		 System.out.println("java");
+
+	}
+	 
+	 public static void main(String[] args) {
+		Sample s = new Sample();
+		s.java();
+		s.oracle();
+	}
+}
