095b3bcd1578e2dc8fb5fc8019c92d717ffd6bfd
dhanu <dhanuram03@gmail.com>
2019-10-20 20:14:26 -0700

    lss

--- src/org/test/Project.java
+++ src/org/test/Project.java
@@ -1,10 +1,9 @@
 package org.test;
 
 public class Project {
-private void add1() {
-	// TODO Auto-generated method stub
 
-<<<<<<< HEAD
+
+
 	private void hello() {
 		// TODO Auto-generated method stub
 
@@ -31,9 +30,7 @@ gghg*/
 
 
 
-=======
-}
->>>>>>> 302e7463dfaf359a4684b5dbdd365c667c3f7019
+
 }
 
 
